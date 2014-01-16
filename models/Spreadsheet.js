b6bd17ab3d060061bb702c21746286eabe82fc93
Joe Moses <jnmoses@gmail.com>
2014-01-15 20:50:32 -0600

    added worksheet model

--- models/Spreadsheet.js
+++ models/Spreadsheet.js
@@ -1,7 +1,42 @@
-var worksheet = {
-  columns: ['A1','B1'],
-  data: [
-    ['value','vaule'],
-    ['value','vaule']
-  ]
+/**
+ * @fileOverview A Worksheet model.
+ * @name Spreadsheet
+ *
+ * Example model:
+ *   var worksheet = {
+ *     columns: ['A1','B1'],
+ *     data: [
+ *       ['value','vaule'],
+ *       ['value','vaule']
+ *     ]
+ *   }
+ *
+ *
+ * @constructor
+ */
+function WorksheetModel(args) {
+  var SELF = this;
+
+  /** An array of column names
+   * @type Array
+   */
+  this.columns = [];
+
+  /** An array of arrays containg values.
+   * @type Array
+   */
+  this.data = [];
+
+  // init
+  (function() {
+    if (args) {
+      if (args.columns) {
+        SELF.columns = args.columns;
+      }
+    }
+  })();
+
+  return SELF;
 }
+
+exports.Worksheet = WorksheetModel;
