4ef3551d45263c20cc602f0febf227c01885a191
Joe Moses <jnmoses@gmail.com>
2014-02-11 22:49:20 -0600

    started creating event model

--- models/Event.js
+++ models/Event.js
@@ -1,4 +1,35 @@
-var event = 
+/**
+ * @fileOverview An Event model.
+ * @name Event
+ *
+ * @constructor
+ */
+function EventModel(args) {
+  var SELF = this;
+
+  /** Summary of the event.
+   * @type String
+   */
+  this.summary = args.summary;
+
+  /** The location of the event.
+   * @type String
+   */
+  this.location = args.location;
+
+  /** The starting date/time of the event.
+   * @type Date
+   */
+  this.start = new Date(args.start);
+
+  /** The ending date/time of the event.
+   * @type Date
+   */
+  this.end = new Date(args.end);
+}
+exports.Event = EventModel;
+
+var event =
 {
   "data": {
     "id": "0",
--- models/index.js
+++ models/index.js
@@ -1,2 +1,3 @@
 exports.Worksheet = require('./Spreadsheet').Worksheet;
 exports.Document = require('./Document').Document;
+exports.Event = require('./Event').Event;
